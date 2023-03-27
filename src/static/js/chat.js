97dd8eed25d21f592e5a4dfb49db965ea2893bcc
Etan S <dr.whaleman@googlemail.com>
2023-03-26 22:01:53 -0500

    worked on chat window, bubbles now appropriately sized based on message length but grade icon doesn't go where I want it to

--- src/static/js/chat.js
+++ src/static/js/chat.js
@@ -1,8 +1,8 @@
-$('#message-form').submit(function(event) {
+$('#msg-form').submit(function(event) {
     event.preventDefault();
 
     var msg = $('#msg').val();
-    $('#chat-msg').append('<div class="text user">' + msg + '</div>'); // hehe it says "text user". What a frickin' goober, you use text?
+    $('#chat-msg').append('<div class="text-row"><img class="grade" src="../static/img/grade.png"><div class="text user">' + msg + '</div></div>'); // hehe it says "text user". What a frickin' goober, you use text?
     $('#msg').val('');
 
     var chat_window = document.getElementById("chat-window");
