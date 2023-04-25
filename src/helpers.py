1e5817b5390f45d8766b69944053e6a0b692260c
Etan S <dr.whaleman@googlemail.com>
2023-04-25 16:29:33 -0500

    documentation finalization

--- src/helpers.py
+++ src/helpers.py
@@ -10,89 +10,11 @@ def translate(text, native, target):
 
 
 # #key is on my desktop in PASSCODES.txt file
-# with open('C:\\Users\\javos\\Desktop\\PASSCODE.txt') as f:
+# with open('C:\\Users\\eschillinger\\Desktop\\PASSCODE.txt') as f:
 #     lines = f.readlines()
 #     key = lines[0].strip()
 
 # openai.api_key = key
-
-
-temp_grades = [{
-        'data' : 'Это место переводит текст, что под находится когда GPT ответит.',
-        'grade_msg' : '<p>Spelling: 0/10. Bad response.</p>\n<p>Grammar: 0/10. Equally bad response. You\'re just not that guy.</p>',
-        'spelling' : 0,
-        'grammar' : 0
-    },
-    {
-        'data' : 'Вам невозможно это читать. Так, мне можно сказать что-либо, что я хочу.',
-        'grade_msg' : '<p>Spelling: 8/10. Random placeholder text here bruh bruh bruh</p>\n<p>Grammar: 7/10. Bruh buh bruh bruh bruh, bruh bruh bruh.</p>',
-        'spelling' : 8,
-        'grammar' : 7
-    },
-    {
-        'data' : 'Сегодня утром я купил очень дорогое молоко. Просто такое дорогое, что тебе невозможно оно полагать.',
-        'grade_msg' : '<p>Spelling: 10/10. There were no spelling mistakes in your response.</p>\n<p>Grammar: 10/10. Your response was well-formed, and does not contain any grammatical errors.</p>',
-        'spelling' : 10,
-        'grammar' : 10
-    }]
-
-
-def gradeResponse(user_input):
-    #ask the system to grade the user
-    response = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",
-        messages = [
-            {
-                "role" : "system",
-                "content" : "Please explicitly grade the student's performance out of 10. Explicitly state each of the 4 subscores (Vocabulary, Grammar, Spelling, Relevance to the topic) for the response each out of 10 as well. Then explain why you gave them that score."
-            },
-            {
-                "role" : "user",
-                "content" : user_input
-            },
-        ]
-    )
-
-    #output the grade (debugging tool)
-    #print(response.choices[0].message.content)
-
-    #parse the grade response for the scores of vocab, grammar, spelling, and relevance int VocabGrade, GrammarGrade, SpellingGrade, and RelevanceGrade
-
-    #get the grade
-    grade = response.choices[0].message.content
-
-    #get the vocab grade
-    VocabGrade = grade[grade.find("Vocabulary: ") + 12]
-    VocabGrade += grade[grade.find("Vocabulary: ") + 13]
-    #if VocabGrade has a / in it then delete it
-    if VocabGrade.find("/") != -1:
-        VocabGrade = VocabGrade[0]
-
-    #get the grammar grade
-    GrammarGrade = grade[grade.find("Grammar: ") + 9]
-    GrammarGrade += grade[grade.find("Grammar: ") + 10]
-    #if GrammarGrade has a / in it then delete it
-    if GrammarGrade.find("/") != -1:
-        GrammarGrade = GrammarGrade[0]
-
-    #get the spelling grade
-    SpellingGrade = grade[grade.find("Spelling: ") + 10]
-    SpellingGrade += grade[grade.find("Spelling: ") + 11]
-    #if SpellingGrade has a / in it then delete it
-    if SpellingGrade.find("/") != -1:
-        SpellingGrade = SpellingGrade[0]
-
-    #get the relevance grade
-    RelevanceGrade = grade[grade.find("Relevance to the topic: ") + 24]
-    RelevanceGrade += grade[grade.find("Relevance to the topic: ") + 25]
-    #if RelevanceGrade has a / in it then delete it
-    if RelevanceGrade.find("/") != -1:
-        RelevanceGrade = RelevanceGrade[0]
-
-    #get everything from "Exlpaination: " to the end of the string and store it in explanation
-    explanation = grade[grade.find("Relevance to the topic: ") + 28:]
-    
-    return VocabGrade, GrammarGrade, SpellingGrade, RelevanceGrade, explanation
 
 
 ###############################################
